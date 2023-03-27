d386257199d41d983d218dbc4199706e9a3ef2ff
Rahul Talukdar <42152385+mr-talukdar@users.noreply.github.com>
2023-03-27 16:33:52 +0530

    added the dynamic listing

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,7 @@
 import ExpenseDate from "./ExpenseDate";
-import { useState } from "react"; //states import
 import "./Expenseitem.css";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title); //States Logic
-  
-
-  const clickHandler = () => {
-    setTitle("Updated");
-    console.log("clicked!");
-  };
-
   return (
     <div className="expense-item">
       <div>
@@ -18,10 +9,9 @@ function ExpenseItem(props) {
       </div>
 
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Click Me!</button>
     </div>
   );
 }
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -82,7 +82,7 @@ const ExpenseForm = (props) => {
           <input
             type="date"
             min="2019-01-01"
-            max="2022-12-31"
+            max="2050-12-31"
             value={enteredDate}
             onChange={dateChangeHandler}
           />
